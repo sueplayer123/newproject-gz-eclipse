664ed592348eb53e33665066db9eede0ee9f3224
Administrator <Administrator@USER-20201019XT>
2020-11-01 15:57:00 +0800

    2020.11.1周日工程实现拍照原图显示成功

--- src/com/example/bottommenu_vp_imgv_tv/fragment/ChatFragment.java
+++ src/com/example/bottommenu_vp_imgv_tv/fragment/ChatFragment.java
@@ -2,7 +2,6 @@ package com.example.bottommenu_vp_imgv_tv.fragment;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,9 +9,11 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import com.example.bottommenu_vp_imgv_tv.R;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -39,29 +40,41 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ChatFragment extends Fragment {
-
-	private static final int Request_take_photo = 100;
+	private static final int REQ_THUMB = 222;
+	// private static final int REQ_GALLERY = 333;
+	// private static final String TAG = "MainActivity";
+	private static final int REQ_TAKE_PHOTO = 444;
+	Button mThumbnail, mFullSize;
+	ImageView mImageView;
+	private Uri photoURI3;
+	// private static final int Request_take_photo = 100;
 	private Button button1;
 	private Button button2;
 	private Button button3;
 	private Button button4;
-	private Button btn6;
+	// private Button btn6;
 	private Button main_btn;
 	private TextView main_tv;
-	private ImageView imgv1;
+	// private ImageView imgv1;
 	private EditText editText1;
 	private WebView webView1;
 	public static SQLiteDatabase database;
 	private static String stString1;
 	private static String stString2;
 	private static String stString3;
-	private Uri mImageUri;
-	private File imageFile;
-	private String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/capture/";
-	private String fileName;
+	// private Uri mImageUri;
+	// private File imageFile;
+	// private String path =
+	// Environment.getExternalStorageDirectory().getAbsolutePath() + "/capture/";
+	// private String fileName;
+	/*
+	 * private String cameraPath =
+	 * 
+	 * Environment.getExternalStorageDirectory() + File.separator +
+	 * Environment.DIRECTORY_DCIM + File.separator + "Camera" + File.separator;
+	 */
 
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
 			@Nullable Bundle savedInstanceState) {
@@ -79,25 +92,25 @@ public class ChatFragment extends Fragment {
 		tabHost.addTab(tabHost.newTabSpec("tab1")
 				.setIndicator("查询流程", getResources().getDrawable(R.drawable.ic_launcher)).setContent(R.id.view1));
 		tabHost.addTab(tabHost.newTabSpec("tab2")
-				.setIndicator("信息更新", getResources().getDrawable(R.drawable.ic_launcher)).setContent(R.id.view2));
+				.setIndicator("拍照分享", getResources().getDrawable(R.drawable.ic_launcher)).setContent(R.id.view2));
 		tabHost.addTab(tabHost.newTabSpec("tab3")
-				.setIndicator("咨询专家", getResources().getDrawable(R.drawable.ic_launcher)).setContent(R.id.view3));
+				.setIndicator("对接微信", getResources().getDrawable(R.drawable.ic_launcher)).setContent(R.id.view3));
 		tabHost.addTab(tabHost.newTabSpec("tab4")
-				.setIndicator("记录评价", getResources().getDrawable(R.drawable.ic_launcher)).setContent(R.id.view4));
+				.setIndicator("更多待定", getResources().getDrawable(R.drawable.ic_launcher)).setContent(R.id.view4));
 
 		button1 = (Button) getActivity().findViewById(R.id.button1);
 		button2 = (Button) getActivity().findViewById(R.id.button2);
 		button3 = (Button) getActivity().findViewById(R.id.button3);
 		button4 = (Button) getActivity().findViewById(R.id.button4);
 		main_btn = (Button) getActivity().findViewById(R.id.main_btn);
-		btn6 = (Button) getActivity().findViewById(R.id.btn6);
+		// btn6 = (Button) getActivity().findViewById(R.id.btn6);
 		main_tv = (TextView) getActivity().findViewById(R.id.main_tv);
-		imgv1 = (ImageView) getActivity().findViewById(R.id.imgv1);
+		// imgv1 = (ImageView) getActivity().findViewById(R.id.imgv1);
 		editText1 = (EditText) getActivity().findViewById(R.id.editText1);
 		webView1 = (WebView) getActivity().findViewById(R.id.webView1);
 
 		database = openDatabase();
-
+		init();
 		button1.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -160,40 +173,38 @@ public class ChatFragment extends Fragment {
 			}
 		});
 
-		btn6.setOnClickListener(new OnClickListener() {
+		/*
+		 * btn6.setOnClickListener(new OnClickListener() {
+		 * 
+		 * @Override public void onClick(View v) { if
+		 * (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+		 * SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss"); Date date =
+		 * new Date(System.currentTimeMillis()); fileName = format.format(date);
+		 * 
+		 * File path =
+		 * Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+		 * File outputImage = new File(path, fileName + ".jpg"); try { if
+		 * (outputImage.exists()) { outputImage.delete(); } outputImage.createNewFile();
+		 * } catch (IOException e) { e.printStackTrace(); } Intent intent = new
+		 * Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		 * intent.addCategory(Intent.CATEGORY_DEFAULT); if (outputImage != null) { if
+		 * (Build.VERSION.SDK_INT >= 24) {
+		 * intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); mImageUri =
+		 * FileProvider.getUriForFile(getActivity(),
+		 * "cn.fonxnickel.officialcamerademo.fileprovider", outputImage); } else {
+		 * 
+		 * mImageUri = Uri.fromFile(outputImage); }
+		 * 
+		 * } intent.putExtra(MediaStore.Images.Media.ORIENTATION, 0);
+		 * intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+		 * startActivityForResult(intent, Request_take_photo);
+		 * 
+		 * }
+		 * 
+		 * else { Toast.makeText(getActivity(), "没有检测到SD卡", Toast.LENGTH_LONG).show(); }
+		 * } });
+		 */
 
-			@Override
-			public void onClick(View v) {
-				if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-
-					File file = new File(path);
-					if (!file.exists()) {
-						file.mkdir();
-					}
-					fileName = String.valueOf(System.currentTimeMillis()) + ".jpg";
-					imageFile = new File(path + fileName);
-					Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-					intent.addCategory(Intent.CATEGORY_DEFAULT);
-					if (imageFile != null) {
-						if (Build.VERSION.SDK_INT >= 24) {
-							intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-							mImageUri = FileProvider.getUriForFile(getActivity(),
-									"cn.fonxnickel.officialcamerademo.fileprovider", imageFile);
-						} else {
-
-							mImageUri = Uri.fromFile(imageFile);
-						}
-
-					}
-					intent.putExtra(MediaStore.Images.Media.ORIENTATION, 0);
-					intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
-					startActivityForResult(intent, Request_take_photo);
-
-				} else {
-					Toast.makeText(getActivity(), "没有检测到SD卡", Toast.LENGTH_LONG).show();
-				}
-			}
-		});
 	}
 
 	@Override
@@ -259,65 +270,184 @@ public class ChatFragment extends Fragment {
 		getActivity().sendBroadcast(mediaScanIntent);
 	}
 
-	private void takePhoto() {
-		Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		if (takePhotoIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+	/*
+	 * private void takePhoto() { Intent takePhotoIntent = new
+	 * Intent(MediaStore.ACTION_IMAGE_CAPTURE); if
+	 * (takePhotoIntent.resolveActivity(getActivity().getPackageManager()) != null)
+	 * {
+	 * 
+	 * imageFile = createImageFile(); if (imageFile != null) { if
+	 * (Build.VERSION.SDK_INT >= 24) {
+	 * takePhotoIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); mImageUri =
+	 * FileProvider.getUriForFile(getActivity(),
+	 * "cn.fonxnickel.officialcamerademo.fileprovider", imageFile); } else {
+	 * 
+	 * mImageUri = Uri.fromFile(imageFile); }
+	 * takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+	 * startActivityForResult(takePhotoIntent, Request_take_photo); } } }
+	 */
 
-			imageFile = createImageFile();
-			if (imageFile != null) {
-				if (Build.VERSION.SDK_INT >= 24) {
-					takePhotoIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-					mImageUri = FileProvider.getUriForFile(getActivity(),
-							"cn.fonxnickel.officialcamerademo.fileprovider", imageFile);
-				} else {
+	/*
+	 * private File createImageFile() { if
+	 * (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+	 * String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new
+	 * Date()); String imageFileName = "JPEG_" + timeStamp + "_"; String storagePath
+	 * = Environment.getExternalStorageDirectory().getAbsolutePath() + "/capture/";
+	 * 
+	 * File storageDir = new File(storagePath); if (!storageDir.exists()) {
+	 * storageDir.mkdirs(); }
+	 * 
+	 * try { imageFile = File.createTempFile(imageFileName, ".jpg", storageDir); }
+	 * catch (IOException e) { e.printStackTrace(); } } else {
+	 * Toast.makeText(getActivity(), "没有检测到SD卡", Toast.LENGTH_LONG).show(); } return
+	 * imageFile;
+	 * 
+	 * }
+	 */
 
-					mImageUri = Uri.fromFile(imageFile);
+	/*
+	 * @Override public void onActivityResult(int requestCode, int resultCode,
+	 * Intent data) {
+	 * 
+	 * super.onActivityResult(requestCode, resultCode, data); if (requestCode ==
+	 * Request_take_photo && resultCode == Activity.RESULT_OK) { try {
+	 * 
+	 * Bitmap bitmap = BitmapFactory
+	 * .decodeStream(getActivity().getContentResolver().openInputStream(mImageUri));
+	 * imgv1.setImageBitmap(bitmap); galleryAddPic(mImageUri); } catch
+	 * (FileNotFoundException e) { e.printStackTrace(); } }
+	 * 
+	 * }
+	 */
+	private void init() {
+		if (Build.VERSION.SDK_INT >= 23) {
+			requestPermissions(new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE }, 200);
+		}
+
+		mThumbnail = (Button) getActivity().findViewById(R.id.thumbnail);
+		mFullSize = (Button) getActivity().findViewById(R.id.fullSize);
+		// mAddGallery = (Button) getActivity().findViewById(R.id.addGallery);
+		mImageView = (ImageView) getActivity().findViewById(R.id.imageView);
+
+		mThumbnail.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {// 判断是否有相机应用
+					startActivityForResult(takePictureIntent, REQ_THUMB);
 				}
-				takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
-				startActivityForResult(takePhotoIntent, Request_take_photo);
 			}
-		}
+		});
+
+		mFullSize.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				// Ensure that there's a camera activity to handle the intent
+				if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {// 判断是否有相机应用
+					// Create the File where the photo should go
+					File photoFile = null;
+					try {
+						photoFile = createImageFile();// 创建临时图片文件
+
+					} catch (IOException ex) {
+						ex.printStackTrace();
+					}
+					// Continue only if the File was successfully created
+					if (photoFile != null) {
+						// FileProvider 是一个特殊的 ContentProvider 的子类，
+						// 它使用 content:// Uri 代替了 file:/// Uri. ，更便利而且安全的为另一个app分享文件
+						Uri photoURI3 = FileProvider.getUriForFile(getActivity(), "com.youga.fileprovider", photoFile);
+
+						takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI3);
+						startActivityForResult(takePictureIntent, REQ_TAKE_PHOTO);
+					}
+				}
+			}
+		});
+
 	}
 
-	private File createImageFile() {
-		if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-			String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-			String imageFileName = "JPEG_" + timeStamp + "_";
-			String storagePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/capture/";
+	String mCurrentPhotoPath;
 
-			File storageDir = new File(storagePath);
-			if (!storageDir.exists()) {
-				storageDir.mkdirs();
-			}
-
-			try {
-				imageFile = File.createTempFile(imageFileName, ".jpg", storageDir);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else {
-			Toast.makeText(getActivity(), "没有检测到SD卡", Toast.LENGTH_LONG).show();
-		}
-		return imageFile;
-
+	private File createImageFile() throws IOException {
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CHINA).format(new Date());
+		String imageFileName = "JPEG_" + timeStamp + "_";
+		// .getExternalFilesDir()方法可以获取到 SDCard/Android/data/你的应用的包名/files/
+		// 目录，一般放一些长时间保存的数据
+		File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+		// 创建临时文件,文件前缀不能少于三个字符,后缀如果为空默认未".tmp"
+		File image = File.createTempFile(imageFileName, /* 前缀 */
+				".jpg", /* 后缀 */
+				storageDir /* 文件夹 */
+		);
+		mCurrentPhotoPath = image.getAbsolutePath();
+		return image;
 	}
+
+	/*
+	 * String mPublicPhotoPath;
+	 * 
+	 * private File createPublicImageFile() throws IOException { File path =
+	 * Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM); //
+	 * Create an image file name
+	 * 
+	 * String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+	 * Locale.CHINA).format(new Date()); String imageFileName = "JPEG_" + timeStamp;
+	 * File image = File.createTempFile(imageFileName, 前缀 ".jpg", 后缀 path 文件夹 );
+	 * mPublicPhotoPath = image.getAbsolutePath(); return image; }
+	 */
+	/*
+	 * private void galleryAddPic() { Intent mediaScanIntent = new
+	 * Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE); File f = new
+	 * File(mPublicPhotoPath); Uri photoURI3 =
+	 * FileProvider.getUriForFile(getActivity(), "com.youga.fileprovider", f);
+	 * mediaScanIntent.setData(photoURI3);
+	 * getActivity().sendBroadcast(mediaScanIntent); }
+	 */
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
 		super.onActivityResult(requestCode, resultCode, data);
-		if (requestCode == Request_take_photo && resultCode == Activity.RESULT_OK) {
-			try {
+		switch (requestCode) {
+		case REQ_THUMB:// 返回结果
+			if (resultCode != Activity.RESULT_OK)
+				return;
+			Bundle extras = data.getExtras();
+			Bitmap imageBitmap = (Bitmap) extras.get("data");
+			mImageView.setImageBitmap(imageBitmap);
+			break;
+		case REQ_TAKE_PHOTO:// 返回结果
+			if (resultCode != Activity.RESULT_OK)
+				return;
 
-				Bitmap bitmap = BitmapFactory
-						.decodeStream(getActivity().getContentResolver().openInputStream(mImageUri));
-				imgv1.setImageBitmap(bitmap);
-				galleryAddPic(mImageUri);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
+			// Get the dimensions of the View
+			int targetW = mImageView.getWidth();
+			int targetH = mImageView.getHeight();
+
+			// Get the dimensions of the bitmap
+			BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+			bmOptions.inJustDecodeBounds = true;
+			BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+			int photoW = bmOptions.outWidth;
+			int photoH = bmOptions.outHeight;
+
+			// Determine how much to scale down the image
+			int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+			// Decode the image file into a Bitmap sized to fill the View
+			bmOptions.inJustDecodeBounds = false;
+			bmOptions.inSampleSize = scaleFactor;
+			bmOptions.inPurgeable = true;
+
+			Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+			mImageView.setImageBitmap(bitmap);
+			galleryAddPic(photoURI3);
+			break;
+
 		}
-
 	}
 
 	private void readHtmlFormAssets() {
